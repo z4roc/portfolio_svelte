5f5852552a1aade4a3557d3a921fffcd782535a4
z4roc <arthuraktamirov@gmail.com>
2023-08-14 16:38:09 +0200

    landing complete

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,36 @@
-<script lang="ts">				
-    import "../app.css"
-    let src = "./images/icon.png"
+<script lang="ts">
+  import "../app.css";
+  let src = "./images/logoWhite.png";
 </script>
 
-<div class=" h-screen bg-bgdark">
-    <div class="p-6 ">
-        <div class="navbar bg-gradient-to-r from-purple-500 to-blue-800 opacity-75 text-opacity-100">
-            <div class="flex-1">
-                <a class="btn btn-ghost normal-case text-2xl" href="/">
-                    <img src={src} alt="icon" height="50" width="50"/>
-                    <p>aktamirov.de</p>
-                </a>
-                
-            </div>
-            <div class="flex-none">
-                <ul class="menu menu-horizontal px-1">
-                  <li><a href="/projects" class=" text-white text-xl">Projects</a></li>
-                  <li>
-                    <a href="/about" class="text-white text-xl">About</a>
-                  </li>
-                </ul>
-              </div>
-        </div>
+<div class="bg-background">
+  <video
+    src="./videos/bg.mp4"
+    class="object-cover fixed w-full h-full"
+    autoplay
+    loop
+    muted
+  >
+    <track kind="captions" />
+  </video>
+  <div class="p-6">
+    <div
+      class="navbar relative bg-gradient-to-r from-primary to-secondary opacity-50 text-opacity-100"
+    >
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-2xl" href="/">
+          <img {src} alt="icon" height="200" width="200" />
+        </a>
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li><a href="/projects" class=" text-white text-xl">Projects</a></li>
+          <li>
+            <a href="/about" class="text-white text-xl">About</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <slot/>
+  </div>
+  <slot />
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,18 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+      },
       borderWidth: {
         1: "1px",
       },
       colors: {
-        bgdark: "#222222",
+        'text': '#eff2e8',
+        'background': '#030302',
+        'primary': '#3c3257',
+        'secondary': '#190b42',
+        'accent': '#6d5b9f',
       },
     },
   },
